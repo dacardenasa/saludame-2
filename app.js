d2f212b394276db6266bc713ebc3e02875772226
dacardenasa <diego.cardenasaleg@gmail.com>
2020-07-28 18:48:17 -0500

    Delete action capitalize

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.get("/user/:name", (req, res) => {
   let reqParamName = req.params.name;
   if (!reqParamName) reqParamName = 'Desconocido!';
-  reqParamName = reqParamName.replace(/^\w/, c => c.toUpperCase());
+  // reqParamName = reqParamName.replace(/^\w/, c => c.toUpperCase());
   res.send(`<h1>Hola ${reqParamName}!</h1>`);
 });
 
