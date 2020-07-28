b6dbaf52d90117b947cd9792de8a7e7f2c7d3326
dacardenasa <diego.cardenasaleg@gmail.com>
2020-07-28 17:55:50 -0500

    Add module capitalizeString

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const app = express();
+const capitalizeString = require("./capitalize");
 
 app.get("/user/:name", (req, res) => {
   let reqParamName = req.params.name;
-  if (!reqParamName || reqParamName.length === 0) reqParamName = 'Desconocido!';
-  
-  reqParamName = reqParamName.split('');
-  reqParamName.every( (letter, index) => { 
-    if (index !== 0) return false;
-
-    if (letter.match(/[a-z]/)) {
-      reqParamName[index] = letter.toUpperCase();
-    }
-  });
-
-  res.send(`<h1>Hola ${reqParamName.join('')}!</h1>`);
+  if (!reqParamName) reqParamName = 'Desconocido!';
+  res.send(`<h1>Hola ${capitalizeString(reqParamName)}!</h1>`);
 });
 
 app.listen(3000, () => console.log("Listening on port: 3000"));
