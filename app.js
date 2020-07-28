d3bfc1be1e906864f3d9de9faa3d39a29a3878ee
dacardenasa <diego.cardenasaleg@gmail.com>
2020-07-28 18:54:13 -0500

    Update route get

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/user/:name", (req, res) => {
+app.get("/makers/:name", (req, res) => {
   let reqParamName = req.params.name;
   if (!reqParamName) reqParamName = 'Desconocido!';
-  // reqParamName = reqParamName.replace(/^\w/, c => c.toUpperCase());
+  reqParamName = reqParamName.replace(/^\w/, c => c.toUpperCase());
   res.send(`<h1>Hola ${reqParamName}!</h1>`);
 });
 
