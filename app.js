3b9fde1e7923c28beec24e2381196053625672c1
dacardenasa <diego.cardenasaleg@gmail.com>
2020-07-28 18:41:18 -0500

    Delete module capitalizaeString to use regular expression in the same app file

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const capitalizeString = require("./capitalize");
 
 app.get("/user/:name", (req, res) => {
   let reqParamName = req.params.name;
   if (!reqParamName) reqParamName = 'Desconocido!';
-  res.send(`<h1>Hola ${capitalizeString(reqParamName)}!</h1>`);
+  reqParamName = reqParamName.replace(/^\w/, c => c.toUpperCase());
+  res.send(`<h1>Hola ${reqParamName}!</h1>`);
 });
 
 app.listen(3000, () => console.log("Listening on port: 3000"));
